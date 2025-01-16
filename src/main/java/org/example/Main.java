1135a0d598ca859ad86c1ebd68b6aef1b51a9f42
Bryan Andika S <bryan.andika77@gmail.com>
2025-01-16 09:30:24 +0700

    sending folder PR10

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] arrayString = {"Mitsubishi", "Tesla", "Honda", "Nissan", "Kia", "Hyundai"};
+        System.out.println(Arrays.toString(arrayString));
+    }
+}
